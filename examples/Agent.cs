a68982f0fb480ca9f5293e96771616f516245f9f
Leon Fretter <leonfretter@web.de>
2023-09-12 12:35:28 +0200

    Finish example

--- examples/Agent.cs
+++ examples/Agent.cs
@@ -1,7 +1,48 @@
 using Godot;
+using System;
 
 [GlobalClass]
 public partial class Agent : Node {
     [Export]
-    public Godot.Collections.Array<UtilityAIOption> Options = new();
+    public Godot.Collections.Array<UtilityAIOption> Options;
+
+    [Export]
+    public UtilityAIContext Context;
+
+    private Label _actionLabel;
+
+    public override void _Ready() {
+        _actionLabel = GetNode<Label>("CanvasLayer/HBoxContainer/Sliders/CurrentActionLabel");
+        foreach (var option in Options) {
+            option.Context = Context;
+        }
+    }
+
+    public override void _Process(double delta) {
+        var bestOptionScore = .0f;
+        string bestAction = "idle";
+
+        foreach (var option in Options) {
+            var score = option.Evaluate();
+            if(score > bestOptionScore) {
+                bestOptionScore = score;
+                // In this example, the behavior-name is used as action-name.
+                bestAction = option.Behavior.Name;
+            }
+        }
+
+        _actionLabel.Text = bestAction;
+    }
+
+    public void SetNutrition(float value) {
+        Context.Set("nutrition", value / 100f);
+    }
+
+    public void SetHydration(float value) {
+        Context.Set("hydration", value / 100f);
+    }
+
+    public void SetEnergy(float value) {
+        Context.Set("energy", value / 100f);
+    }
 }
